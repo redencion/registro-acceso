08b00528163e4eb47be14749bd69894f6abf471d
Alexis Canchica <alenux1@gmail.com>
2018-08-31 15:52:16 -0400

    Agregando Cambios controlador para el registro de organizacion

--- app/controllers/organizacion_controller.rb
+++ app/controllers/organizacion_controller.rb
@@ -1,2 +1,18 @@
 class OrganizacionController < ApplicationController
+  before_action  :set_organizacion, only: [:consultar, :actulizar, :editar, :eliminar]
+
+  def index
+    @organizacion = Organizacion.all  
+  end
+  
+  def mostrar
+  end
+
+  def set_organizacion
+    @org = Organizacion.find(params[:id])
+  end
+
+
+
+
 end
