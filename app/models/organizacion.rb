82f6752cf70fce8f408157002d0998769d6af59a
Alexis Canchica <alenux1@gmail.com>
2018-09-03 15:07:57 -0400

    Asociacion de las modelos Persona y Organizacion + validación registro

--- app/models/organizacion.rb
+++ app/models/organizacion.rb
@@ -2,4 +2,6 @@ class Organizacion < ApplicationRecord
   self.table_name = 'Organizacion'
   validates :nombre, presence: {message: "¡Es un campo requerido!"}
   validates :nombre, uniqueness: {message: "¡Ya se encuetra registrado!"}
+
+  has_many  :persona
 end
--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,5 +1,7 @@
 class Persona < ApplicationRecord
   self.table_name = 'Persona'
+  validates :nombre, :apellido, :cedula, :credencial,  presence: {message: "\t EL campo es requerido"}
+  validates :cedula, :credencial, uniqueness: {message: "\t¡El dato ya existe!"}
 
   belongs_to :organizacion
 end
